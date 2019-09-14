e714effd95ba3e16a50d3f5f884321f794311281
kirillTarasov <tarasovk2001@yandex.ru>
2019-09-14 18:52:45 +0500

    initial commit

---
+++ variables/main.js
@@ -0,0 +1,13 @@
+let algebra = 5, geography = 4, physics = 3;
+let averageScore = (algebra + geography + physics) / 3;
+
+console.log(averageScore);
+
+let name = "Kirill";
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
+
+let x = 2, y = 22, z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
