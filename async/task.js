3e0ba934e314a2ade4cb1af752cd8dd90497f462
kirillTarasov <tarasovk2001@yandex.ru>
2019-10-02 20:52:56 +0500

    async fix

--- async/task.js
+++ async/task.js
@@ -1,13 +1,27 @@
-let systemTime = '23:10';
 
 function setDailyRhythm(wakeUpTime, bedIime) {
+    let systemTime = '';
+    if ((new Date).getHours() < 10) {
+        systemTime += '0' + (new Date).getHours();
+    } else {
+        systemTime += (new Date).getHours();
+    }
+    if ((new Date).getMinutes() < 10) {
+        systemTime += ':0' + (new Date).getMinutes();
+    } else {
+        systemTime += ':'+ (new Date).getMinutes();
+    }
+    console.log(systemTime);
+    systemTime = '23:00';
     const goodMorning = () => console.log('Доброе утро!'),
 	checkTimeforWakeUp = setAlarm(wakeUpTime, goodMorning);
     const goodNight =  () => console.log('Спокойной ночи!'),
     checkTimeForBed = setAlarm(bedIime, goodNight);
-    setInterval(checkTimeforWakeUp, 1000,systemTime);
-    setInterval(checkTimeForBed, 1000, systemTime);
+    checkTimeForBed(systemTime);
+    checkTimeforWakeUp(systemTime);
 } 
+
+
 
 function setAlarm(time, callback) {
     return systemTime => {
@@ -15,4 +29,4 @@ function setAlarm(time, callback) {
     } 
 }
 
-setDailyRhythm('07:00', '23:00');
+setInterval(setDailyRhythm, 1000, '07:00', '23:00');
