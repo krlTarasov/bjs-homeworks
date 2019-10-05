f4eb733e17ccd9d92d1486062ee0cdf165d22bed
kirillTarasov <tarasovk2001@yandex.ru>
2019-10-05 16:49:47 +0500

    async fix 3

--- async/task.js
+++ async/task.js
@@ -1,13 +1,10 @@
-
 function setDailyRhythm(wakeUpTime, bedIime) {
+    const goodMorning = () => console.log('Доброе утро!'),
+    checkTimeforWakeUp = setAlarm(wakeUpTime, goodMorning);
+    const goodNight =  () => console.log('Спокойной ночи!'),
+    checkTimeForBed = setAlarm(bedIime, goodNight);
     setInterval(() => {
         let systemTime = String(new Date).substring(16, 21);
-        console.log(systemTime, wakeUpTime, bedIime);
-        systemTime = '07:00';
-        const goodMorning = () => console.log('Доброе утро!'),
-        checkTimeforWakeUp = setAlarm(wakeUpTime, goodMorning);
-        const goodNight =  () => console.log('Спокойной ночи!'),
-        checkTimeForBed = setAlarm(bedIime, goodNight);
         checkTimeForBed(systemTime);
         checkTimeforWakeUp(systemTime);
     }, 1000)
@@ -21,7 +18,4 @@ function setAlarm(time, callback) {
     } 
 }
 
-setDailyRhythm('07:00', '23:00')
-
-let a = String(new Date).substring(16, 21);
-console.log(a);
+setDailyRhythm('07:00', '23:00');
